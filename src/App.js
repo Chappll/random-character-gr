65ec7ffe173a7308dce7b39ac11b854fe941a162
Chappll <68033975+Chappll@users.noreply.github.com>
2021-04-19 00:34:28 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+
+import './App.css';
+import './Button.css';
+import dog from './heropics/DogGunfire.PNG'
+import cat from './heropics/CatGunfire.PNG'
+import bird from './heropics/BirdGunfire.PNG'
+import tiger from './heropics/TigerGunfire.PNG'
+import gunfire from './heropics/GunfireLogo.PNG'
+import backB from './heropics/bgBlack.PNG'
+import React, { useState } from 'react';
+
+function App() {
+  const[characters,setCharacters] = useState([{name: 'Crown Prince', picture: cat}, {name: 'Ao Bai', picture: dog}, {name: 'Qing Yan', picture: bird}, {name: 'Lei Luo', picture: tiger}])
+  const[randChar, setRandChar] = useState(Math.floor(Math.random() * (3 - 0 + 1)) + 0)
+  const[again, setAgain] = useState(1)
+
+  function randomInteger(min, max) {
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (num == randChar) {
+      setAgain(again + 1)
+    }
+    else{
+      setAgain(1)
+    }
+    return num
+  }
+
+  function showText() {
+    if (again > 0) {
+      return(characters[randChar].name + ' ' + again)
+    }
+    return(characters[randChar].name) 
+  }
+
+  return (
+    <div className="App">
+      <img src={gunfire} height={200} width={400}></img>
+      <div class="flex-container" style={{flexDirection:'column', justifyContent:'center'}}> 
+          <header style={{color:'white'}}>{characters[randChar].name + ' ' + (again>1?('x' + again):'')}</header>
+          <img src={characters[randChar].picture} height={200} width={200}></img>
+          <div>
+            <button className="button" onClick={()=> setRandChar(randomInteger(0,3))}> Randomise Character</button>
+          </div>   
+          <img src={backB} height={100}></img>
+      </div>
+    </div>
+  );
+}
+
+export default App;
