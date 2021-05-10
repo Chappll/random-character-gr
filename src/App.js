7a774b9a624d484c5525096b5c975b4034541c94
Chappll <68033975+Chappll@users.noreply.github.com>
2021-05-10 01:31:25 +0100

    Loading icon added

--- src/App.js
+++ src/App.js
@@ -6,22 +6,30 @@ import cat from './heropics/CatGunfire.PNG'
 import bird from './heropics/BirdGunfire.PNG'
 import tiger from './heropics/TigerGunfire.PNG'
 import gunfire from './heropics/GunfireLogo.PNG'
+import Loader from "react-loader-spinner";
 import React, { useState } from 'react';
 
 function App() {
   const[characters] = useState([{name: 'Crown Prince', picture: cat}, {name: 'Ao Bai', picture: dog}, {name: 'Qing Yan', picture: bird}, {name: 'Lei Luo', picture: tiger}])
   const[randChar, setRandChar] = useState(Math.floor(Math.random() * (3 - 0 + 1)) + 0)
+  const [loading,setLoading] = useState(true)
   const[again, setAgain] = useState(1)
 
   function randomInteger(min, max) {
+    setLoading(true)
     const num = Math.floor(Math.random() * (max - min + 1)) + min;
     if (num === randChar) {
       setAgain(again + 1)
+      setLoading(false)
     }
     else{
       setAgain(1)
     }
     return num
+  }
+
+  const handleImageLoaded = () => {
+    setLoading(false)
   }
 
 
@@ -31,11 +39,11 @@ function App() {
         <img src={gunfire} alt="logo" height={600} width={1200}></img>
         <div class="flex-container" style={{flexDirection:'column', justifyContent:'center'}}> 
             <header style={{color:'white', fontSize:'40px'}}>{characters[randChar].name + ' ' + (again>1?('x' + again):'')}</header>
-            <img src={characters[randChar].picture} alt={characters[randChar].name} height={500} width={500}></img>
+            <Loader z-index="-1" type="Puff" color="#00BFFF" height={100} width={100} visible={loading} />
+            <img onLoad={handleImageLoaded} src={characters[randChar].picture} alt={characters[randChar].name} height={500} width={500}></img>       
             <div>
               <button className="button" onClick={()=> setRandChar(randomInteger(0,3))}> Randomise Character</button>
-            </div>   
-            
+            </div>            
         </div>
       </div>
     </div>
